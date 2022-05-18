b70f47a04a391769a228635c0a495ddd53424b44
MoriCesar <cesinha_lowlife@hotmail.com>
2022-05-18 13:40:54 +0900

    movimentacao e pulo
    o pulo tem um problema....vai ser resolvido na aula 141

---
+++ git_Plataforma2D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class PlayerController : MonoBehaviour 
+{
+	public float jumpForce = 550;
+	public Transform groundCheck;
+	public float groundRadius = 0.1f;
+	public LayerMask groundLayer;
+
+	[SerializeField]
+	private float walkSpeed;
+
+	private Rigidbody2D rb;
+	private Vector2 newMovement;
+
+	private bool facingRight = true;
+
+	private bool jump;
+	private bool grounded;
+	private bool doubleJump;
+
+	private void Awake() 
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
+
+		if (grounded)
+			doubleJump = false;
+	}
+
+	private void FixedUpdate() 
+	{
+		rb.velocity = newMovement;
+
+		if (jump)
+        {
+			jump = false;
+			rb.velocity = Vector2.zero;
+			rb.AddForce(Vector2.up * jumpForce);
+
+			if (!doubleJump && !grounded)
+            {
+				doubleJump = true;
+            }
+        }
+	}
+
+	public void Jump()
+    {
+		if(grounded || !doubleJump)
+			jump = true;
+    }
+
+	public void Move(float direction)
+	{ 
+		float currentSpeed = walkSpeed;
+		newMovement = new Vector2(direction * currentSpeed, rb.velocity.y);
+
+		if (facingRight && direction < 0)
+		{
+			Flip();
+		}
+		else if(!facingRight && direction > 0)
+		{
+			Flip();
+		}
+	}
+
+	void Flip()
+	{
+		facingRight = !facingRight;
+
+		transform.Rotate(0, 180, 0);
+	}
+}
